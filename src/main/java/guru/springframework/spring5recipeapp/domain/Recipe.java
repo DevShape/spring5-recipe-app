878128cc88d5f06fadb373642ec42034fba01743
DevShape <42915957+DevShape@users.noreply.github.com>
2021-07-08 17:31:02 +0200

    Created Ingredient and set up OneToMany relationship from Recipe

--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -18,6 +19,10 @@ public class Recipe {
 
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
     @Lob
     private Byte[] image;
 
